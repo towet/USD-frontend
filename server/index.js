ad4eae023918548ce1cef96ddfec1bd351433498
User <user@example.com>
2025-03-02 08:57:31 +0300

    Initial commit with frontend code and CORS updates

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://visa-expert.netlify.app', 'https://global-visa-experts.netlify.app'],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
 
 const config = {
